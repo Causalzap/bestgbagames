85c65c51d8cf8ddeb1e05185734b152007b2cb56
Causalzap <causalzap@gmail.com>
2025-08-11 15:22:32 +0800

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   siteUrl: 'https://www.bestgbagames.com',
   generateRobotsTxt: true,
+  outDir: 'public',  // 可选，指定输出目录
   exclude: ['/secret', '/admin/*'],
   robotsTxtOptions: {
     policies: [
