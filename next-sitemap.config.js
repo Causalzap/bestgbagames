e8399729c12a894e7b3c45380e666bc655edc574
Causalzap <causalzap@gmail.com>
2025-08-11 20:32:53 +0800

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,22 +1,20 @@
-// 根目录/next-sitemap.config.js
+、// next-sitemap.config.js
 module.exports = {
   siteUrl: 'https://www.bestgbagames.com',
   generateRobotsTxt: true,
-  changefreq: 'daily',  
-  priority: 0.7, 
-  sitemapSize: 5000 , 
-  exclude: ['/secret', '/admin/*'],
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/tmp', '/private']
-      },
-      {
-        userAgent: 'Googlebot-Image',
-        disallow: ['/images']
-      }
-    ]
+  changefreq: 'weekly',
+  priority: 0.8,
+  sitemapSize: 10000, // 确保生成单个文件
+  exclude: [
+    '/admin',
+    '/private' // 只需排除真实存在的敏感路径
+  ],
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: path === '/' ? 'daily' : 'monthly',
+      priority: path === '/' ? 1.0 : 0.7,
+      lastmod: new Date().toISOString()
+    }
   }
-};
+}
